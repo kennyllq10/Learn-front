27e51946c85b53180baff41d28d69302fb7f1723
kennyllq10 <lvluqin1995@163.com>
2020-04-03 15:56:14 +0800

    完成了登录功能

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes :[
+    // 重定向
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+//挂载路由导航守卫
+router.beforeEach((to,from,next) => {
+  //to将要访问的路径
+  //from从哪个路径跳转
+  //next是一个函数，表示放行
+  //   next（） 放行    next（’/login‘）强制跳转
+  if(to.path == '/login') return next();
+  //获取token
+  const tokenstr = window.sessionStorage.getItem('token');
+  if(!tokenstr) return next('/login');
+  next();
+})
+
+export default router
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './index.js'
+import './plugins/element.js'
+//导入全局样式
+import './assets/css/global.css'
+import axios from 'axios'
+//配置请求的跟路径
+axios.defaults.baseURL ='http://106.12.11.162:8888/api/private/v1/'
 
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
